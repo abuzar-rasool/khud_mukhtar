8c48bd9bf9a8fe7cf1199cb2c65789b0b08ade23
Abuzar Rasool <availabuzar@gmail.com>
2020-02-13 14:08:02 +0500

    Added Screen: Chat Added Models User ChatMessageModel Added Images for chat screen to

---
+++ lib/src/screens/home_screen_chat.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:khud_mukhtar/constants/colors.dart';
+
+import 'package:khud_mukhtar/src/widgets/recent_chats.dart';
+
+class HomeScreenChat extends StatefulWidget {
+  @override
+  _HomeScreenChatState createState() => _HomeScreenChatState();
+}
+
+class _HomeScreenChatState extends State<HomeScreenChat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[300],
+      appBar: AppBar(
+        backgroundColor: Colors.pink[300],
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Chats',
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.pink[300],
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  RecentChats(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
