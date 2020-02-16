e7337cfee2f916bf9b553c8ca556767e94dfc618
fatima moin <fm03771@st.habib.edu.pk>
2020-02-16 17:19:54 +0500

    Onboarding Screens added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:khud_mukhtar/src/screens/home_screen.dart';
 import 'package:khud_mukhtar/src/screens/home_screen_chat.dart';
+import 'package:khud_mukhtar/src/screens/onboarding_1.dart';
 import 'package:khud_mukhtar/src/screens/profile_screen.dart';
 import 'package:khud_mukhtar/src/screens/profile_seller.dart';
 import 'package:khud_mukhtar/src/screens/rating.dart';
@@ -9,22 +10,19 @@ import 'package:khud_mukhtar/src/screens/service_single.dart';
 import 'package:khud_mukhtar/src/screens/service_single2.dart';
 import 'package:khud_mukhtar/src/screens/signup_screen.dart';
 import './src/screens/login_screen.dart';
+import './src/screens/onboarding_1.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      routes: <String, WidgetBuilder>{
-        '/services_details': (BuildContext context) => new ServiceDetailsPage(),
-        '/rating':(BuildContext context)=> new Rating(),
-      },
       title: 'Splash',
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: OnboardingPage(),
 
       // HomeScreenChat() chat screen
       //ServiceSinglePage2() teaching page
       //ServiceSinglePage() logodesign
-    )
+    ),
   );
 
 }
